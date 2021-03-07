f87f41f7fc1f32f0564c9afaa595b39fc158a1f9
PixPlix <dev@pixplix.com>
2021-03-06 23:08:13 -0500

    Periodico Visual
    VIsual

--- periodico.php
+++ periodico.php
@@ -4,6 +4,60 @@
     <!-- Incluyendo navegador -->
     <?php include "plantilla/navbar.php"; ?>
    
+    <div style="margin-top:100px;"></div>
+      <div class="contentDivPeriodico" >
+
+        <div class="col-md-12 row">
+          <div class="col-md-9">
+            <div class="col-md-12" style="background-color:white;border-radius: 15px;">
+              <br>
+              <div class="row ">
+                  <div class="col-md-5">
+                      <div class="col-md-12 divCentrado">
+                        <img src="images/img-project/ImgPer6043da8173043.jpg" class="divImg">
+                        <div class="centrado"><h4>El estado colombiano definitivamente se separa en tres países.</h4></div>
+                      </div>
+                  </div>
+                  <div class="col-md-7">
+                    <h5>Este es el contra titulo</h5>
+                    <p>Textooooooo</p>
+                    <footer><a href="#">Más Detalles</a></footer>
+                    <br>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="col-md-12" style="background-color:white;border-radius: 15px;">
+              <br>
+              <article>
+                <b>Anónimo</b>
+                <footer>Comentarios ... . .. . .</footer>
+              </article>
+              <br>
+              <article>
+                <b>Anónimo</b>
+                <footer>Comentarios ... . .. . .</footer>
+              </article>
+              <br>
+
+              <textarea class="form-control" placeholder="Realizar Comentario "></textarea>
+              <button class="btn btn-primary">Comentar</button>
+              <br>
+              <br>
+            </div>
+            <br>
+          </div>
+          <br>
+          <div class="col-md-12">
+            <div class="col-md-12" style="">
+              hola 3
+            </div>
+          </div>
+        </div>
+        
+      </div>
+
     <?php require_once 'plantilla/footer.php'; ?>
     <a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
     <?php include 'plantilla/script.php' ?>
