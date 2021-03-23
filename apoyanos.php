e0eae7a51e378fa3cf7293ff76529c2ce5a1cb4e
PixPlix <dev@pixplix.com>
2021-03-23 08:33:43 -0500

    Apoyanos
    Visual

--- apoyanos.php
+++ apoyanos.php
@@ -5,38 +5,45 @@
     <?php include "plantilla/navbar.php"; ?>
     <div style="margin-top:100px;"></div>
     
-  <div class="col-md-12" align="center">
+  <div class="col-md-12 marginApoyanosTop" align="center">
     <h1 id="tittleApoyanos" style="color:white"></h1>
   </div>
   
   
-  <div class="col-md-12" style="text-align:justify;">
-    <div class="divImgApoyanos">
-      <img id="imgApoyanos"> 
+  <div class="col-md-12 row" style="text-align:justify;">
+    <div class="col-md-8">
+      <h3 id="descApoyanos" style="color:white;text-transform: none !important;"></h3>
     </div>
-    <h3 id="descApoyanos" style="color:white"></h3>
+    <div class="col-md-4">
+      <div class="">
+        <img id="imgApoyanos"> 
+      </div>
+    </div>
+    <div class="col-md-12">
+      <div class="col-md-12" align="center">
+        <h3 style="color:white">VAKI</h3>
+        <div class="col-md-12" align="center">
+          <iframe id="videoApoyanos" width="650" height="400" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12">
+      <br>
+      <h3 style="color:white">Comentarios</h3>
+      <div class="col-md-12" style="background-color:white;border-radius: 15px; overflow: scroll; max-height: 470px;">
+        <br>
+        <textarea class="form-control" placeholder="Realizar Comentario " id="comentarioApoyanos"></textarea>
+        <br>
+        <button class="btn btn-primary pull-right" id="btnComentarioApoyanos" onclick="_Apoyanos.ingresarComent()">Comentar</button>
+        <br><br>
+        <div id="divOpinionesApoyanos"></div>
+      </div>
+      <br>
+    </div>
   </div>
   
-  <div class="col-md-12" align="center">
-    <h3 style="color:white">VAKI</h3>
-  </div>
 
-  <div class="col-md-12" align="center">
-    <iframe id="videoApoyanos" width="650" height="400" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </div>
 
-  <div class="col-md-3 col-sm-12">
-    <br>
-    <div class="col-md-12" style="background-color:white;border-radius: 15px; overflow: scroll; max-height: 470px;">
-      <br>
-      <textarea class="form-control" placeholder="Realizar Comentario " id="comentarioApoyanos"></textarea>
-      <br>
-      <button class="btn btn-primary pull-right" id="btnComentarioApoyanos" onclick="_Apoyanos.ingresarComent()">Comentar</button>
-      <br><br>
-      <div id="divOpinionesApoyanos"></div>
-    </div>
-    <br>
-  </div>
   
     <div style="margin-top:100px;"></div>
     <div style="margin-top:100px;"></div>
